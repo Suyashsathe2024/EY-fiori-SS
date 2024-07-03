58211dff8bf17f2cc9b6cbf5b42a8e4eeb820140
SUYASH SATHE <suyash.sathe@in.ey.com>
2024-07-03 14:11:19 +0000

    my stable app

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ats.ss.travelproc.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
